f344acfd7540181119ffc8e1b3aa4bf92b6da220
Yongkun Li <indusdesign@163.com>
2019-09-13 09:54:30 -0400

    updated

---
+++ Falling_Balls/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  reset();
+}
+
+function reset(){
+  clear();
+  cord_x = 0;
+  cord_y = 0;
+  fall_flag = 0;
+  g = 0.2;
+  v = 0;
+}
+
+function draw() {
+  if (cord_x>=windowWidth || cord_y>=windowWidth){
+    v = -v;
+    //reset();
+  }
+  
+  if (fall_flag==1){
+    v+=g;
+    cord_y+=v;
+  }
+  
+  if (cord_x!=0 && cord_y!=0){
+    clear();
+    fill(0);
+    circle(cord_x, cord_y, 10);
+  }
+}
+
+function touchStarted() {
+  clear();
+  v=0;
+  cord_x = mouseX;
+  cord_y = mouseY;
+  fall_flag=1;
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# My P5 works here
+
+This page is used for testing and showing my P5 works
+
+## Index
+
+- [Falling Balls](./Falling_Balls/index.html)
+- [Device_Moved](./Device_Moved/index.html)
