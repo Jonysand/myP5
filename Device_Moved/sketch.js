7f8111ed3c37900287cc8c422173363f6265e534
Yongkun Li <indusdesign@163.com>
2019-09-13 22:39:55 -0400

    updated

--- Device_Moved/sketch.js
+++ Device_Moved/sketch.js
@@ -1,16 +1,50 @@
+width = 400;
+height = 400;
+radius = 10
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  reset();
 }
 
-let value = 0;
+function reset(){
+  clear();
+  cord_x = 0;
+  cord_y = 0;
+  fall_flag = 0;
+  g = 0.2;
+  gx = 0;
+  guy = 0;
+  vx = 0;
+  vy = 0;
+}
 
 function draw() {
-  fill(value);
-  rect(25, 25, 50, 50);
-}
-function deviceMoved() {
-  value = value + 5;
-  if (value > 255) {
-    value = 0;
+  clear();
+  if (cord_x>=windowWidth-radius || cord_x<=radius){
+    vx = vx;
   }
+  if (cord_y>=windowHeight-radius || cord_y<=radius){
+    vy = vy;
+  }
+  
+  gx = g * Math.sin(radians(rotationX))
+  gy = g * Math.sin(radians(rotationY))
+  
+  if (fall_flag==1){
+    vx += gx;
+    vy += gy;
+    cord_x += vx;
+    cord_y -= vy;
+    fill(0);
+    circle(cord_x, cord_y, radius);
+  }
+}
+
+function touchStarted() {
+  reset();
+  cord_x = mouseX;
+  cord_y = mouseY;
+  fall_flag=1;
 }
